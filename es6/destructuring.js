4fc0c3c87e153805bc8d9ccde90dbd45c8d36577
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-10-04 16:39:33 -0300

    update js

---
+++ es6/destructuring.js
@@ -0,0 +1,5 @@
+const cars = [ "porsche", "ferrari", "lamborghini"]
+
+const [car1, car2, car3] = cars
+
+console.log(car1)
---
+++ es6/spreadOperator.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+
+const a3 = [...a1, ...a2];
+
+const a4 = [7,8,9]
+const a5 = [...a3, ...a4];
+
+console.log(a5)
