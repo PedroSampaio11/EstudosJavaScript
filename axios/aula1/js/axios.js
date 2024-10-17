7ee33bf52fd2efa11e4d6d858666e5a14684fd35
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-10-16 21:02:28 -0300

    treino axios

--- axios/aula1/js/axios.js
+++ axios/aula1/js/axios.js
@@ -1,11 +1,42 @@
+/** @format */
 
-const getData = async () => {
+const pegar = async () => {
   try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-    console.log(response.data)
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(response.data);
+    return response.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+pegar();
 
-getData()
+const imprimir = async () => {
+  try {
+    const resposta = await pegar();
+
+    console.log(resposta)
+
+    resposta.forEach(usuarios => {
+      const div = document.createElement("div")
+      container.appendChild(div)
+
+      const nomes = document.createElement("h1")
+      nomes.textContent = ` Nome: ${usuarios.name}`
+
+      const emails = document.createElement("h2")
+      emails.textContent = `Email: ${usuarios.email}`
+      emails.style.color = "pink"
+
+      div.appendChild(nomes)
+      div.appendChild(emails)
+
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+imprimir()
