a3ec7a49bc02d6b9498d76010e24577d2639d1eb
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-10-24 18:25:25 -0300

    add js

---
+++ axios/aula1/js/global.js
@@ -0,0 +1,4 @@
+/** @format */
+
+axios.defaults.headers.common["accept"] = "application/json";
+axios.defaults.headers.common["authorization"] = "meutokendoapp";
--- axios/aula1/js/index.js
+++ axios/aula1/js/index.js
@@ -1,4 +1,3 @@
-
 // pegando os dados
 const getData = async () => {
   try {
@@ -7,44 +6,40 @@ const getData = async () => {
     );
 
     console.log(response);
-    
-    return response.data
+
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-getData()
+getData();
 
 // imprimir na tela
-
-const container = document.querySelector("#users_container")
+const container = document.querySelector("#users_container");
 
 const printData = async (params) => {
-  const data = await getData()
-  console.log(data)
+  const data = await getData();
+  console.log(data);
 
   // percorre por todos users
-  data.forEach(user => {
+  data.forEach((user) => {
+    // cria a div
+    const div = document.createElement("div");
+    const nomes = user.name;
+    const label = document.createElement("h2");
 
-    // cria a div 
-    const div = document.createElement("div")
-    const nomes = user.name
-    const label = document.createElement("h2")
-    
+    label.textContent = `username da  ${nomes} :`;
+    div.appendChild(label);
+    label.style.color = "black";
 
-    label.textContent = `username da  ${nomes} :`
-    div.appendChild(label)
-    label.style.color = "black"
+    const nameUser = document.createElement("p");
+    nameUser.textContent = user.username;
+    nameUser.style.color = "blue";
+    nameUser.style.fontSize = "18px";
+    div.appendChild(nameUser);
 
-
-    const nameUser = document.createElement("p")
-    nameUser.textContent = user.username
-    nameUser.style.color = "blue"
-    nameUser.style.fontSize = "18px"
-    div.appendChild(nameUser)
-
-    container.appendChild(div)
+    container.appendChild(div);
   });
-}
-printData()
+};
+printData();
